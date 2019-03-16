909131ee88a0ae52fdfcb91b06a0655b01cf1bfb
Rob Romito <rob@robertromito.com>
2019-03-16 15:17:20 -0400

    Output tweak

--- keybankcsv.py
+++ keybankcsv.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 import sys
+import os
+
+screen_rows, screen_cols = os.popen('stty size', 'r').read().split()
+title_width = 40 
+amount_width = 10 
 
 def split_by_account(csv):
     accounts = []
@@ -24,14 +29,17 @@ def amount_to_money(amount_string):
         return 0.0
 
 def sum_transactions_for(keybank_csv):
-    transaction_amounts = [amount_to_money((f.split(",")[1]))
-                           for f in keybank_csv
-                           if '"' in f]
-    return sum(transaction_amounts)
+    return sum([amount_to_money((f.split(",")[1]))
+                for f in keybank_csv
+                if '"' in f])
 
 def write_means(title, means):
-    print("{0:50}\033[1;{2}m{1:10,.2f}\033[1;m"
-          .format(title, means, "41" if means < 0 else ""))
+    return "{0:{3}}\033[1;{2}m{1:{4},.2f}\033[1;m".format(
+            title, 
+            means, 
+            "41" if means < 0 else "", 
+            title_width,
+            amount_width)
 
 if len(sys.argv) < 2:
    print(f'usage: keybankcsv.py [glob path of keybank csv files dir]')
@@ -39,16 +47,21 @@ if len(sys.argv) < 2:
 
 print("Analysys of Key Bank csv download files:")
 
+results = []
+
 total_means = 0
 for csv in sys.argv[1:]:
     with open(csv, 'r') as f:
         transactions = f.readlines()
         accounts = split_by_account(transactions)
-        print(f'\n\033[1;44m{csv:60}\033[1;m\n')
+        results.append(f'\n\033[1;44m{csv:{screen_cols}}\033[1;m\n')
         for account in accounts:
-            write_means(account[0].rstrip(), sum_transactions_for(account))
+            account_means = sum_transactions_for(account)
+            results.append(write_means(account[0].rstrip(),account_means))
         csv_means = sum_transactions_for(transactions)
-        write_means("Total", csv_means)
+        results.append(write_means("Total", csv_means))
         total_means += csv_means
-        print("\n")
-        write_means("Grand Total", total_means)
+        results.append("")
+        results.append(write_means("Grand Total", total_means))
+
+[print(r) for r in results]
